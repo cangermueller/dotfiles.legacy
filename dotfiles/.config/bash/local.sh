2660664e9fc29391a81c4d0c85615bc876f883f3
Christof Angermueller <cangermueller@gmail.com>
2026-08-28 09:23:13 -0700

    Update configs

--- dotfiles/.config/bash/local.sh
+++ dotfiles/.config/bash/local.sh
@@ -1,18 +1,21 @@
-# MacPorts
-append_path "/opt/local/bin"
-append_path "/opt/local/sbin"
+alias gs="glogin && gcert --reuse_sso_cookie"
+alias gj="ssh g1 -t -- /bin/bash -c 'gcertstatus -nocheck_ssh -check_remaining=1h || gcert; \$SHELL'"
+
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-__conda_setup="$('/Users/christofa/python/conda/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+__conda_setup="$('/opt/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
 if [ $? -eq 0 ]; then
     eval "$__conda_setup"
 else
-    if [ -f "/Users/christofa/python/conda/anaconda3/etc/profile.d/conda.sh" ]; then
-        . "/Users/christofa/python/conda/anaconda3/etc/profile.d/conda.sh"
+    if [ -f "/opt/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/anaconda3/etc/profile.d/conda.sh"
     else
-        export PATH="/Users/christofa/python/conda/anaconda3/bin:$PATH"
+        export PATH="/opt/anaconda3/bin:$PATH"
     fi
 fi
 unset __conda_setup
 # <<< conda initialize <<<
+
